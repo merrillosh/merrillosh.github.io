04867de1ee39cfdee6ddd6ac99f7e0a93b271171
Merrill O'Shaughnessy <merrill.oshaughnessy@duke.edu>
2023-01-10 14:05:13 -0600

    owl

--- assets/script.js
+++ assets/script.js
@@ -11769,3 +11769,21 @@ var _gsScope = "undefined" != typeof module && module.exports && "undefined" != 
           t("#page").smoothState(i);
       });
   })(jQuery);
+
+  let slider = tns({
+    container: '.my-slider',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    responsive: {
+      640: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      },
+      1400: {
+        items: 4
+      }
+    }
+  });
