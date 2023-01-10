ec59d856458c2ffeae5e803ab03653ce2a71990f
Merrill O'Shaughnessy <merrill.oshaughnessy@duke.edu>
2023-01-10 14:12:42 -0600

    owl

--- assets/script.js
+++ assets/script.js
@@ -11774,16 +11774,5 @@ var _gsScope = "undefined" != typeof module && module.exports && "undefined" != 
     container: '.my-slider',
     items: 1,
     slideBy: 'page',
-    autoplay: true,
-    responsive: {
-      640: {
-        items: 2
-      },
-      1000: {
-        items: 3
-      },
-      1400: {
-        items: 4
-      }
-    }
+    autoplay: true
   });
